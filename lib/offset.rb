76c1b3c7f7cea2ab478925be7b62676c7d3dc33e
Nico <nicovigil1@gmail.com>
2018-11-02 12:51:03 -0600

    changes initialize for offset to be able to take date as an optional arg

--- lib/offset.rb
+++ lib/offset.rb
@@ -1,14 +1,15 @@
 require "date"
 class Offset
   attr_reader :date, :date_int, :alphabet, :key
-  def initialize(key)
-    @date = Date.today
+  def initialize(key, date = Date.today)
+    @date = date
     @date_int = @date.strftime("%d%m%y").to_i
     @alphabet = ("a".."z").to_a << " "
     @key = key
   end
 
   def gears
+
     gears = []
     gear_1 = @key[0..1].to_i
   end
