27f0fac0d27299e11858989833eb4affc85fe1ff
Nico <nicovigil1@gmail.com>
2018-11-03 15:56:29 -0600

    adds key class test for use by the encryption class

---
+++ test/encrypt_test.rb
@@ -0,0 +1,12 @@
+require "./test/test_helper"
+
+class EncryptTest < Minitest::Test
+  def setup
+    @encrypt = Encrypt.new
+  end
+
+  def test_it_exists
+    assert_instance_of Encrypt, @encrypt
+  end
+
+end
---
+++ test/key_test.rb
@@ -0,0 +1,26 @@
+require "./test/test_helper"
+
+class KeyTest < Minitest::Test
+  def setup
+    @key = Key.new
+  end
+
+  def test_it_exists
+    assert_instance_of Key, @key
+  end
+
+  def test_it_can_generate_a_key
+    skip
+    key_range = (1000..99_999)
+    assert_equal Integer, @key.key.class
+    assert key_range.include?(@key.key)
+  end
+
+  def test_it_can_generate_0_as_first_number
+    skip
+    key_range = (1000..9_999)
+    assert_equal Integer, @key.test_0_first.class
+    assert_equal 0, @key.to_s[0]
+  end
+
+end
