7568b5f148fb73b2fb04206c05a3d31817f34350
Nico <nicovigil1@gmail.com>
2018-11-03 18:16:21 -0600

    removes useless method who's sole purpose was to test that a key could be generated with a 0 at the beginning

--- lib/key.rb
+++ lib/key.rb
@@ -11,17 +11,17 @@ class Key
     end
   end
 
-  def test_0_first
-    num = Random.new
-    key = num.rand(1000..9_999)
-    #same as above - just ensures number can start with 0
-    if key.to_s.length == 4
-      key = "0" + key.to_s
-      key
-    else
-      key.to_s
-    end
+  # looking for another way to test this
 
-  end
+  # def test_0_first
+  #   num = Random.new
+  #   key = num.rand(1000..9_999)
+  #   #same as above - just ensures number can start with 0
+  #   if key.to_s.length == 4
+  #     key = "0" + key.to_s
+  #     key
+  #   end
+  #
+  # end
 
 end
--- test/key_test.rb
+++ test/key_test.rb
@@ -14,11 +14,13 @@ class KeyTest < Minitest::Test
     assert_equal String, @key.generate_key.class
     assert key_range.include?(@key.generate_key.to_i)
   end
+  # there's gotta be a better way to test this
 
-  def test_it_can_generate_0_as_first_number
-    key_range = (1000..9_999)
-    assert_equal String, @key.test_0_first.class
-    assert_equal "0", @key.test_0_first[0]
-  end
+  # def test_it_can_generate_0_as_first_number
+  #   key_range = (1000..9_999)
+  #   assert_equal String, @key.test_0_first.class
+  #   assert_equal "0", @key.test_0_first[0]
+  #   assert key_range.include?(@key.test_0_first.to_i)
+  # end
 
 end
