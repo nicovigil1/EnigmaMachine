19d2dd1798d3b73f0e7021e68d88fef6a8168652
Nico <nicovigil1@gmail.com>
2018-11-04 13:16:17 -0700

    adds encrypt test for cli

--- test/encrypt_test.rb
+++ test/encrypt_test.rb
@@ -2,15 +2,21 @@ require "./test/test_helper"
 
 class EncryptTest < Minitest::Test
   def setup
-    @encrypt = Encrypt.new
+    @encrypt = Encrypt.new(message)
   end
 
   def test_it_exists
-    assert_instance_of Encrypt, @encrypt
+    assert_instance_of Encrypt, @encrypt("hey now")
   end
 
   def test_it_has_key
     assert_equal "12345", @encrypt.key
   end
+
+  def test_it_has_a_date
+    assert_equal String, @encrypt.date.class
+    assert_equal 6, @encrypt.date.length
+  end
+
 
 end
